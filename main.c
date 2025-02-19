d541d26bc73052c7123f9c2ceaf33f8c138d0e05
Ridwan Hidayatullah <24.ridwanhidayatullah.9b@gmail.com>
2025-02-19 13:04:06 +0700

    add cmake setting for c lang version 11

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
-int main(int, char**){
+int main(void) // Explicitly declare main() with void
+{
     printf("Hello, from tosparser!\n");
+    return 0; // Recommended for portability
 }
