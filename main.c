5da3bc0c8de9146def37e0168b35347985d35b71
Ridwan Hidayatullah <24.ridwanhidayatullah.9b@gmail.com>
2025-02-20 11:14:02 +0700

    add ipf parser and decompress

--- main.c
+++ main.c
@@ -1,7 +1,48 @@
+
 #include "ipf.h"
 
-int main(void) // Explicitly declare main() with void
+/**
+ * Main function to load and print IPF data.
+ */
+int main(void)
 {
-    printf("Hello, from tosparser!\n");
-    return 0; // Recommended for portability
+
+    char *filename = "char_texture.ipf";
+
+    FILE *ipf_file = fopen(filename, "rb");
+    if (!ipf_file)
+    {
+        perror("Failed to open file");
+        return EXIT_FAILURE;
+    }
+
+    IPF_Root ipf_root = {0};
+
+    if (!load_ipf(&ipf_root, ipf_file))
+    {
+        fclose(ipf_file);
+        return EXIT_FAILURE;
+    }
+
+    print_ipf_details(&ipf_root);
+
+    // Extract the first file
+    uint8_t *extracted_data = NULL;
+    size_t extracted_size = 0;
+
+    if (extract_data(ipf_file, &ipf_root.ipf_file_table[0], &extracted_data, &extracted_size))
+    {
+        printf("Extraction successful.\n");
+        print_hex_dump(extracted_data, 16, 2);
+        free(extracted_data);
+    }
+    else
+    {
+        printf("Extraction failed.\n");
+    }
+
+    free_ipf_file_table(ipf_root.ipf_file_table, ipf_root.ipf_footer.file_count);
+    fclose(ipf_file);
+
+    return EXIT_SUCCESS;
 }
